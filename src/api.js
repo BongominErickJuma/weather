7229d05858482381f65ae2896789658e773cd736
BongominErickJuma <ericbongomin@gmail.com>
2023-09-21 21:12:59 +0300

    protected the api keys

--- src/api.js
+++ src/api.js
@@ -2,11 +2,11 @@ export const geoUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '011719432amsh92e7d71acb3109ep115be5jsn5b9c9f30c01b',
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const weatherUrl = "https://api.openweathermap.org/data/2.5"
 
-export const weatherApi = "d775c6b4eb44324e58a80f7b8ebff08b"
+export const weatherApi = process.env.REACT_APP_WEATHER_API_KEY
